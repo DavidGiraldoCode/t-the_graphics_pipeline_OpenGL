a4604acba0d75789d0e321df0b270c20e68a41b9
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-04-13 05:39:41 +0200

    Removed comments

--- src/shaderClass.cpp
+++ src/shaderClass.cpp
@@ -1,23 +1,5 @@
 #include "shaderClass.h"
-/*
-fragmentCode = get_file_contents(fragmentFile);
-// Reads a text file and outputs a string with everything in the text file
-std::string get_file_contents(const char *filename)
-{
-	std::ifstream in(filename, std::ios::binary);
-	if (in)
-	{
-		std::string contents;
-		in.seekg(0, std::ios::end);
-		contents.resize(in.tellg());
-		in.seekg(0, std::ios::beg);
-		in.read(&contents[0], contents.size());
-		in.close();
-		return (contents);
-	}
-	throw(errno);
-}
-*/
+
 // Vertex Shader source code
 const char *vertexSource = "#version 330 core\n"
 						   "layout (location = 0) in vec3 aPos;\n"
@@ -36,31 +18,6 @@ const char *fragmentSource = "#version 330 core\n"
 // Constructor that build the Shader Program from 2 different shaders
 Shader::Shader(const char *vertexFile, const char *fragmentFile)
 {
-
-	/*
-	std::cout << "Shader Constructor " << vertexFile << std::endl;
-
-	std::string vertexCode;
-	std::string fragmentCode;
-
-	// Read vertexFile and fragmentFile and store the strings
-	try
-	{
-		// vertexCode = get_file_contents(vertexFile);
-		std::cout << vertexCode << std::endl;
-		// fragmentCode = get_file_contents(fragmentFile);
-	}
-	catch (const std::exception &e)
-	{
-		// Handle the exception here
-		std::cerr << "Exception occurred: " << e.what() << std::endl;
-		// You can choose to log the error, display a message to the user, or take any other appropriate action
-	}
-	// const char *vertexSource = vertexCode.c_str();
-	// Convert the shader source strings into character arrays
-	const char *vertexSource = vertexCode.c_str();
-	const char *fragmentSource = fragmentCode.c_str();
-	*/
 
 	// Create Vertex Shader Object and get its reference
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
